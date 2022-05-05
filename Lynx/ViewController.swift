3c9e083b104d24ba0fdeaf44e4e32af17a4e1822
Pietro <pietro.fariello@me.com>
2022-05-05 17:12:31 +0200

    edits

--- Lynx/ViewController.swift
+++ Lynx/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Initial Location
+        let initialLocation = CLLocation(latitude: 37.695670, longitude: 24.060816)
+        mapView.centerToLocation(location: initialLocation)
     }
 
 
 }
 
+private extension MKMapView {
+  func centerToLocation(location: CLLocation, regionRadius: CLLocationDistance = 1000){
+    let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
